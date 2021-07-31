cdaf1d68635a70d549ea8e302cc523e063b7d853
Asim <asim@poptani.com>
2021-07-31 23:27:46 +0100

    Inital ideas for avionics

--- workspaces/avionics/avionics/avionics.ino
+++ workspaces/avionics/avionics/avionics.ino
@@ -1,6 +1,20 @@
+// Need to include Serial??
 
 void setup() {
+    // Setup Serial to talk to the GPS
+    // https://www.u-blox.com/sites/default/files/products/documents/MAX-6_DataSheet_%28GPS.G6-HW-10106%29.pdf
+    Serial1.begin(115200);
+    while (!Serial1);
+    // Setup I2C to talk to the MPL3115A2 - Pressure sensor with altimetry
+    // Setup SPI so we can talk to the radio
+    // Setup SD card ????
+
 }
 
 void loop() {
+    // Get GPS data
+    // Get pressure and altitude
+    // Combnine data to a string or CSV
+    // Send via radio interface
+    // Save to SD card
 }
