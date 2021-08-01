59341a0bfb92fc28b66e3268135597d25e1f210f
Asim <asim@poptani.com>
2021-08-01 18:50:32 +0100

    Added GPS

--- workspaces/avionics/avionics/avionics.ino
+++ workspaces/avionics/avionics/avionics.ino
@@ -1,13 +1,15 @@
 // Need to include Serial??
 #include <Wire.h>
 #include <SPI.h>
+#include <TinyGPS.h>
+TinyGPS gps;
 const int I2C_PRESSURE_AND_ALTIMETER=4
 const int SPI_RADIO_SS=7
 const int SPI_SD_SS=8
 void setup() {
     // Setup Serial to talk to the GPS
     // https://www.u-blox.com/sites/default/files/products/documents/MAX-6_DataSheet_%28GPS.G6-HW-10106%29.pdf
-    Serial1.begin(115200);
+    Serial1.begin(9600);
     while (!Serial1);
     // Setup I2C to talk to the MPL3115A2 - Pressure sensor with altimetry
      Wire.begin(); // join i2c bus (address optional for master)
@@ -22,7 +24,47 @@ void setup() {
 
 void loop() {
     // Get GPS data
-    char * gpsData=Serial1.readString()
+    bool newData = false;
+    unsigned long chars;
+    unsigned short sentences, failed;
+
+    // For one second we parse GPS data and report some key values
+    for (unsigned long start = millis(); millis() - start < 1000;)
+    {
+        while (ss.available())
+        {
+        char c = ss.read();
+        // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+        if (gps.encode(c)) // Did a new valid sentence come in?
+            newData = true;
+        }
+    }
+
+    if (newData)
+    {
+        float flat, flon;
+        unsigned long age;
+        gps.f_get_position(&flat, &flon, &age);
+        Serial.print("LAT=");
+        Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+        Serial.print(" LON=");
+        Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+        Serial.print(" SAT=");
+        Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+        Serial.print(" PREC=");
+        Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+    }
+    
+    gps.stats(&chars, &sentences, &failed);
+    Serial.print(" CHARS=");
+    Serial.print(chars);
+    Serial.print(" SENTENCES=");
+    Serial.print(sentences);
+    Serial.print(" CSUM ERR=");
+    Serial.println(failed);
+    if (chars == 0){
+        Serial.println("** No characters received from GPS: check wiring **");
+    }
     // Get pressure and altitude
     Wire.beginTransmission(I2C_PRESSURE_AND_ALTIMETER); // transmit to device #4
     Wire.write("x is ");        // sends five bytes 
@@ -32,7 +74,6 @@ void loop() {
     // Read pressure and altitude 
     while(Wire.available())    // slave may send less than requested
     {
-        
         char c = Wire.read();    // receive a byte as character
         pressureAndAltitude[index]=c; 
         index=index+1
